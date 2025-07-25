35f603dd509b158e8c830ff875bfe0462b18af41
Aaditya Prakash Kattekola <aadityapra2910@gmail.com>
2025-07-25 10:53:46 -0700

    NVIM with LSP and FZF

--- nvim/lua/kapi/core/keymaps.lua
+++ nvim/lua/kapi/core/keymaps.lua
@@ -21,3 +21,9 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- Code Formatter (Conform)
+keymap.set("n", "<leader>cf",function() require("conform").format() end, {desc="Format Current File"})
+
+-- Lsp
+vim.keymap.set("n","gl", function() vim.diagnostic.open_float() end, {desc="Open Diagnostic in Float"})
